fe87ca35dfdcf56d473945fdca9a39044dc4e26f
Lawrence Leitgib <lawrence11.leitgib@icloud.com>
2026-05-07 00:02:46 +0200

    feat: add a prototype of federated learning
    Co-authored-by: Roberto Goelzer <robertaleG@users.noreply.github.com>
    Co-authored-by: Rodrigo Coelho <racoelhosilva@users.noreply.github.com>

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 sudo apt-get update
 sudo apt-get install screen
 
-pip install torch==2.9.1 torchvision==0.24.1 torchaudio==2.9.1 --index-url https://download.pytorch.org/whl/cu130
-pip install transformers datasets wandb omegaconf jaxtyping kornia
-pip install -U Pillow
-sudo pip uninstall --yes flash_attn
+VENV_PIP="$(dirname "$0")/venv/bin/pip"
+
+"$VENV_PIP" install torch==2.9.1 torchvision==0.24.1 torchaudio==2.9.1 --index-url https://download.pytorch.org/whl/cu130
+"$VENV_PIP" install transformers datasets wandb omegaconf jaxtyping kornia
+"$VENV_PIP" install -U Pillow
+"$VENV_PIP" uninstall --yes flash_attn
