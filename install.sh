47aab2e1835d3ea9d9dc2319a7e7fe7012a0a4d9
Roberto Goelzer <roberto.goelzer@epfl.ch>
2026-06-08 19:00:34 +0200

    chore: cleanup

--- install.sh
+++
@@ -1,9 +0,0 @@
-sudo apt-get update
-sudo apt-get install screen
-
-VENV_PIP="$(dirname "$0")/venv/bin/pip"
-
-"$VENV_PIP" install torch==2.9.1 torchvision==0.24.1 torchaudio==2.9.1 --index-url https://download.pytorch.org/whl/cu130
-"$VENV_PIP" install transformers datasets wandb omegaconf jaxtyping kornia
-"$VENV_PIP" install -U Pillow
-"$VENV_PIP" uninstall --yes flash_attn
